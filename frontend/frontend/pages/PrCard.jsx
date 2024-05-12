56651081b04910ef6d007088aada2307c0d8d7eb
Yunus Eren Türkeri <82608558+LeDauphinn@users.noreply.github.com>
2024-05-13 02:50:27 +0300

    details page completed

--- frontend/frontend/pages/PrCard.jsx
+++ frontend/frontend/pages/PrCard.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const PrCard = ({ prName, prData }) => {
-  // console.log(prName);
-  // console.log(prData);
+  const [contributor, setContributor] = useState(prName);
 
-  return <div>PrCard {prData}</div>;
+  const [prsContributed, setPrsContributes] = useState(
+    prData[0][0] + ": " + prData[0][1]
+  );
+  const [sentimentScore, setSentimentScore] = useState(
+    prData[1][0] + ": " + prData[1][1]
+  );
+
+  useEffect(() => {
+    setPrsContributes(prData[0][0] + ": " + prData[0][1]);
+    setSentimentScore(prData[1][0] + ": " + prData[1][1]);
+    setContributor(prName);
+  }, [prData, prData, prName]);
+
+  return (
+    <div>
+      <div>
+        <div className="card my-2" style={{ width: "100%", backgroundColor: "#fff" }}>
+          <div className="card-body">
+            <h5 className="card-title">Contributor: {contributor}</h5>
+            <p className="card-text">{prsContributed}</p>
+            <p className="card-text">{sentimentScore}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PrCard;
--- frontend/frontend/pages/docs/[...slug]/index.jsx
+++ frontend/frontend/pages/docs/[...slug]/index.jsx
@@ -17,20 +17,38 @@ import Form from "react-bootstrap/Form";
 const Page = () => {
   const [repoData, setRepoData] = useState({});
 
-  const [selectedCommitFilter, setSelectedCommitFilter] = useState(1);
-  const [selectedIssueFilter, setSelectedIssueFilter] = useState(7);
-  const [selectedPrFilter, setSelectedPrFilter] = useState(19);
+  const [selectedCommitFilter, setSelectedCommitFilter] = useState(0);
+  const [selectedPrFilter, setSelectedPrFilter] = useState(0);
+  const [selectedIssueFilter, setSelectedIssueFilter] = useState(0);
 
   const handleCommitFilterChange = (e) => {
     const value = Number(e.target.value);
     setSelectedCommitFilter(value);
   };
 
+  const handlePRFilterChange = (e) => {
+    const value = Number(e.target.value);
+    setSelectedPrFilter(value);
+  };
+
+  const handleIssuesFilterChange = (e) => {
+    const value = Number(e.target.value);
+    setSelectedIssueFilter(value);
+  };
+
   useEffect(() => {
-    applyFilter();
+    applyCommitFilter();
   }, [selectedCommitFilter]);
 
-  const applyFilter = () => {
+  useEffect(() => {
+    applyPRFilter();
+  }, [selectedPrFilter]);
+
+  useEffect(() => {
+    applyIssuesFilter();
+  }, [selectedIssueFilter]);
+
+  const applyCommitFilter = () => {
     let sortedList;
     switch (selectedCommitFilter) {
       case 1:
@@ -126,6 +144,126 @@ const Page = () => {
       default:
       // Handle other cases
     }
+  };
+
+  const applyPRFilter = () => {
+    let combinedList = prDataList.map((data, index) => ({
+      data,
+      name: prNames[index],
+    }));
+
+    switch (selectedPrFilter) {
+      case 1:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[0][1];
+          const bValue = b.data[0][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      case 2:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[1][1];
+          const bValue = b.data[1][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      default:
+      // Handle other cases
+    }
+
+    const sortedPrDataList = combinedList.map((item) => item.data);
+    const sortedPrNames = combinedList.map((item) => item.name);
+
+    setPrDataList(sortedPrDataList);
+    setPrNames(sortedPrNames);
+  };
+
+  const applyIssuesFilter = () => {
+    let combinedList = issueDataList.map((data, index) => ({
+      data,
+      name: issueNames[index],
+    }));
+
+    switch (selectedIssueFilter) {
+      case 1:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[2][1];
+          const bValue = b.data[2][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      case 2:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[1][1];
+          const bValue = b.data[1][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      case 3:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[3][1];
+          const bValue = b.data[3][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      case 4:
+        combinedList.sort((a, b) => {
+          const aValue = a.data[0][1];
+          const bValue = b.data[0][1];
+
+          if (aValue > bValue) {
+            return -1;
+          } else if (aValue < bValue) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        break;
+      default:
+      // Handle other cases
+    }
+
+    const sortedIssueList = combinedList.map((item) => item.data);
+    const sortedIssueName = combinedList.map((item) => item.name);
+
+    setIssueDataList(sortedIssueList);
+    setIssueNames(sortedIssueName);
   };
 
   const [developerNames, setDeveloperNames] = useState([]);
@@ -247,19 +385,33 @@ const Page = () => {
                     )}
                   </Tab>
                   <Tab eventKey="prs" title="PRs">
-                    {!issueDataList || issueDataList.length === 0 ? (
-                      <Spinner animation="border" />
-                    ) : (
-                      issueDataList.map((issueData, idx) => (
-                        <IssueCard
-                          key={idx}
-                          issueName={issueNames[idx]}
-                          issueData={issueData}
-                        />
-                      ))
-                    )}
-                  </Tab>
-                  <Tab eventKey="issues" title="Issues">
+                    <div className="d-flex align-items-center">
+                      <Button
+                        className="btn-teal go-back-btn mb-2"
+                        onClick={() => handleBack()}
+                      >
+                        <IoMdArrowRoundBack />
+                        Back
+                      </Button>
+                      <Form.Select
+                        onChange={(e) => handlePRFilterChange(e)}
+                        defaultValue={selectedPrFilter}
+                        style={{
+                          width: "20rem",
+                          marginLeft: "1rem",
+                          padding: "8px",
+                          textAlign: "center",
+                          fontSize: "16px",
+                        }}
+                        aria-label="Default select example"
+                      >
+                        <option disabled value="0">
+                          Filter by:
+                        </option>
+                        <option value="1">PRs Contributed</option>
+                        <option value="2">Sentiment Score</option>
+                      </Form.Select>
+                    </div>
                     {!prDataList || prDataList.length === 0 ? (
                       <Spinner animation="border" />
                     ) : (
@@ -268,6 +420,48 @@ const Page = () => {
                           key={idx}
                           prName={prNames[idx]}
                           prData={prData}
+                        />
+                      ))
+                    )}
+                  </Tab>
+                  <Tab eventKey="issues" title="Issues">
+                    <div className="d-flex align-items-center">
+                      <Button
+                        className="btn-teal go-back-btn mb-2"
+                        onClick={() => handleBack()}
+                      >
+                        <IoMdArrowRoundBack />
+                        Back
+                      </Button>
+                      <Form.Select
+                        onChange={(e) => handleIssuesFilterChange(e)}
+                        defaultValue={selectedIssueFilter}
+                        style={{
+                          width: "20rem",
+                          marginLeft: "1rem",
+                          padding: "8px",
+                          textAlign: "center",
+                          fontSize: "16px",
+                        }}
+                        aria-label="Default select example"
+                      >
+                        <option disabled value="0">
+                          Filter by:
+                        </option>
+                        <option value="1">Number of Issues Created</option>
+                        <option value="2">Number of Issues Assigned</option>
+                        <option value="3">Number of Issues Resolved</option>
+                        <option value="4">Average Issue Resolution Time</option>
+                      </Form.Select>
+                    </div>
+                    {!issueDataList || issueDataList.length === 0 ? (
+                      <Spinner animation="border" />
+                    ) : (
+                      issueDataList.map((issueData, idx) => (
+                        <IssueCard
+                          key={idx}
+                          issueName={issueNames[idx]}
+                          issueData={issueData}
                         />
                       ))
                     )}
